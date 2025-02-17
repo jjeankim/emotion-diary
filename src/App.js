593def4b1a7b9ad4b955d7966570ab647c6a4cfe
jjeankim <miyukikj35@gmail.com>
2025-02-17 16:36:33 +0900

    Feat: params로 동적 경로 생성

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
+
+function App() {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/new">New</Link>
+      <Link to="/diary">Diary</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
