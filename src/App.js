c749ff1131b9ee029ec5acf17ccc439ddad21bd7
jjeankim <miyukikj35@gmail.com>
2025-02-19 13:58:25 +0900

    Feat: state 추가

--- src/App.js
+++ src/App.js
@@ -3,18 +3,39 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
-import Button from "./components/Button";
-import Header from "./components/Header";
+import Edit from "./pages/Edit";
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  return state
+}
+
+const mockDate = [
+  {
+    id: 1,
+    createDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockDate);
+
   return (
     <>
-    <Header title={"header"} leftChild={<Button text={"left"} />} rightChild={<Button text={"right"}/>} />
-      <Button text={123} type={"NEGATIVE"}></Button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
